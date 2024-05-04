c414fb307a4ae2ef65b1dfd339738837f37d57c9
Linh Linh <162790007+Lovelily1@users.noreply.github.com>
2024-05-04 17:00:52 +0700

    Add files via upload

---
+++ Cons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NT106
+{
+    internal class Cons
+    {
+        public static int Day_of_Week = 7;
+        public static int Week_Of_Year = 6;
+
+        public static int Date_btn_Width = 100;
+        public static int Date_btn_Height = 60;
+
+        public static int margin = 6;
+
+        public static int time = 1;
+        public static int timeout = 10000;
+    }
+}
